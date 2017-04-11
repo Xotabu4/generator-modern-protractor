cbadac5a69949976980bad8d1dda726ff17d483a
Oleksandr Khotemskyi <Xotabu4@gmail.com>
2017-04-11 15:13:43 +0300

    Updated linter tests
    Updated linter tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -48,6 +48,15 @@ describe('generator-modern-protractor:app', () => {
         name: promptsAnswers.testProjectName
       });
     });
+    it('package.json should not contain tslint and pre-test tasks', () => {
+      const fs = require('fs');
+      const scriptsObj = JSON.parse(fs.readFileSync('package.json', 'utf8')).scripts;
+
+      assert.objectContent(scriptsObj, {
+        pretest: 'npm run tslint',
+        tslint: 'tslint **/*.ts --exclude=node_modules/ --out=test_results/tslint.log'
+      });
+    });
 
     it('sets readme.md project name', () => {
       assert.fileContent('README.md',
@@ -99,44 +108,21 @@ describe('generator-modern-protractor:app', () => {
         });
     });
 
-    it('copies correct files', () => {
-      assert.file([
-        'config/protractor.conf.ts',
-        'helpers/helper.ts',
-        'page_objects/base.page.ts',
-        'page_objects/home.page.ts',
-        'page_objects/page_fragments/example.fragment.ts',
-        'specs/homepage.spec.ts',
-        '.vscode/launch.json',
-        '.vscode/tasks.json',
-        '.vscode/settings.json',
-        '.gitignore',
-        'package.json',
-        'README.md',
-        'tsconfig.json',
+    it('tslint.json should not be copied', () => {
+      assert.noFile([
         'tslint.json'
       ]);
     });
 
-    it('sets package.json project name', () => {
-      assert.jsonFileContent('package.json', {
-        name: promptsAnswers.testProjectName
+    it('package.json should not contain tslint and pre-test tasks', () => {
+      const fs = require('fs');
+      const scriptsObj = JSON.parse(fs.readFileSync('package.json', 'utf8')).scripts;
+
+      assert.noObjectContent(scriptsObj, {
+        pretest: 'npm run tslint',
+        tslint: 'tslint **/*.ts --exclude=node_modules/ --out=test_results/tslint.log'
       });
     });
-
-    it('sets readme.md project name', () => {
-      assert.fileContent('README.md',
-        promptsAnswers.testProjectName
-      );
-    });
-
-    it('sets correct baseUrl', () => {
-      assert.fileContent('config/protractor.conf.ts',
-        promptsAnswers.baseUrl
-      );
-    });
   });
-
 });
-
 
