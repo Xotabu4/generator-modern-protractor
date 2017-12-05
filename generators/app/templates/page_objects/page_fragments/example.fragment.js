7ad7c8c8d3e1e28e3afa4ecf59e1b41c371ae35e
Alex Tunick <alexdrakeua@gmail.com>
2017-12-05 23:21:30 +0200

    deleted

--- generators/app/templates/page_objects/page_fragments/example.fragment.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const protractor_element_extend_1 = require("protractor-element-extend");
-class ExampleFragment extends protractor_element_extend_1.BaseFragment {
-}
-exports.ExampleFragment = ExampleFragment;
-//# sourceMappingURL=example.fragment.js.map
